8a07b850d5932a934f6bfce44a41868db99bf226
A.A Ryanta <message.driyant@outlook.com>
2022-09-14 23:27:59 +0700

    Commit - add feature login controller

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -2,7 +2,40 @@ const { checkPasswordHashed, createToken } = require("../helpers");
 const { User, Ticket, OrderDetail } = require("../models");
 
 class LoginController {
-  
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      console.log(req.body);
+      const findUser = await User.findOne({
+        where: {
+          email: email,
+        },
+      });
+      if (!findUser) {
+        throw {
+          name: "INVALID_USERNAME_OR_PASSWORD",
+        };
+      }
+      const isValid = checkPasswordHashed(password, findUser.password);
+      if (!isValid) {
+        throw {
+          name: "INVALID_USERNAME_OR_PASSWORD",
+        };
+      }
+      const payload = {
+        id: findUser.id,
+      };
+      const token = createToken(payload);
+      res.status(200).json({
+        access_token: token,
+        role: findUser.role,
+        email: findUser.email,
+      });
+    } catch (error) {
+      // console.log(error);
+      next(error);
+    }
+  }
 }
 
 module.exports = LoginController;
